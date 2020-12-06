ebcc16247c3ad2f7d269c27dc8d4b0e6c0c46ff8
vichkunina <vichkunina@yandex-team.ru>
2020-12-06 21:55:26 +0300

    внедряет bemg в плагин

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const BemTask = require('bemg/lib/Task');
+
+
 export function activate(context: vscode.ExtensionContext) {
+    const generate = vscode.commands.registerCommand('vs-bem-generate.bemGenerate',
+    async (uri: vscode.Uri) => {
+        const itemsOptions = { prompt: "Enter item names separated by space" };
+        const typesOptions = { prompt: "Enter types names separated by space" };
+        const itemsInput = await vscode.window.showInputBox(itemsOptions);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-bem-generate" is now active!');
+        if(!itemsInput) {
+            return
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-bem-generate.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+        const typesInput = await vscode.window.showInputBox(typesOptions);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-bem-generate!');
-	});
+        if(!typesInput) {
+            return
+        }
 
-	context.subscriptions.push(disposable);
+        try {
+            const task = new BemTask(uri.path);
+
+            task.write(typesInput.split(' '), itemsInput.split(' '));
+        } catch (error) {
+            vscode.window.showErrorMessage(error.message);
+        }
+
+    })
+
+    context.subscriptions.push(generate);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
