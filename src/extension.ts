2e9e616c96e38b55d305d843917ad1c87ded0576
ertema <ertema@yandex-team.ru>
2020-12-18 14:33:08 +0300

    Удаляет лишний return

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,29 @@
 import * as vscode from 'vscode';
 
-const BemTask = require('bemg/lib/Task');
-
+const Task = require('bemg/lib/Task');
 
 export function activate(context: vscode.ExtensionContext) {
     const generate = vscode.commands.registerCommand('vs-bem-generate.bemGenerate',
     async (uri: vscode.Uri) => {
-        const itemsOptions = { prompt: "Enter item names separated by space" };
-        const typesOptions = { prompt: "Enter types names separated by space" };
-        const itemsInput = await vscode.window.showInputBox(itemsOptions);
-
-        if(!itemsInput) {
-            return
-        }
-
+        const itemsOptions = { prompt: "Enter item names separated by space (__elem _mod_val ...etc)" };
+        const typesOptions = { prompt: "Enter types names separated by space (css tsx ...etc)" };
+        
+        const itemsInput = await vscode.window.showInputBox(itemsOptions) || '';
         const typesInput = await vscode.window.showInputBox(typesOptions);
 
         if(!typesInput) {
-            return
+            return;
         }
 
         try {
-            const task = new BemTask(uri.path);
+            const task = new Task(uri.path);
 
             task.write(typesInput.split(' '), itemsInput.split(' '));
         } catch (error) {
             vscode.window.showErrorMessage(error.message);
         }
 
-    })
+    });
 
     context.subscriptions.push(generate);
 }
