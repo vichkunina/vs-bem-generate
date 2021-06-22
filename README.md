2010908675c7213e5035f792963d25d9fdbcc551
Sergey Pushkin <pushkinss@yandex-team.ru>
2021-06-22 14:51:11 +0300

    Bemg init и quickpick (#3)
    - добавляет команду `Bem init` для инициализации дефолтных настроек `bemg`
    - меняет `input` на `quickpick` для выбора типов файлов
      - варианты значений списка формируются по имеющимся шаблонам и учитывают алиасы и ранее введенные типы сущностей (например, если введено только название `__my-element`, то список будет содержать только `css` и `tsx`)
      - есть несколько вариантов выбора:
        - выбрать нужные варианты курсором мыши
        - выбрать все варианты нажав на галку слева от инпута
        - выбрать нужные варианты стрелками на клавиатуре (чтобы поставить галку используется `space`)
        - как и раньше ввести все нужные типы руками (совпаданея будут отмечаться выбранными по мере ввода)
    - обновляет readme и добавляет гифки (по ним понятнее что происходит)

--- README.md
+++ README.md
@@ -1,65 +1,23 @@
 # vs-bem-generate README
 
-This is the README for extension "vs-bem-generate". After writing up a brief description, we recommend including the following sections.
-
+Плагин для генерации файлов с кодом по BEM.
+Для генерации файлов используется [bemg](https://github.yandex-team.ru/ertema/bemg).
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Bem init
+Команда `Bem init` инициализирует bemg с дефолтными настройками. 
 
-For example if there is an image subfolder under your extension project workspace:
+![Bem init](images/init.gif)
 
-\!\[feature X\]\(images/feature-x.png\)
+### Bem generate
+Команда и пункт контекстного меню `Bem generate` в дереве файлов используется для генерации файлов по шаблонам.
+Чтобы сгенерировать файлы, нужно указать типы сущностей bem и выбрать нужные типы шаблонов.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+![Bem init](images/generate.gif)
 
 ### For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+* [vs-bem-generate github repository](https://github.yandex-team.ru/pelican/vs-bem-generate)
+* [bemg github repository](https://github.yandex-team.ru/ertema/bemg)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,94 @@
 import * as vscode from 'vscode';
 
-const Task = require('bemg/lib/Task');
+const TaskGenerate = require('bemg/lib/TaskGenerate');
+const TaskInit = require('bemg/lib/TaskInit');
+const { join, resolve } = require("path");
+const bemNaming = require('bem-naming');
 
 export function activate(context: vscode.ExtensionContext) {
+    const init = vscode.commands.registerCommand('vs-bem-generate.bemInit',
+        async () => {
+            try {
+                const src = join(resolve(__dirname, ".."), "node_modules", "bemg", "bemg");
+                const dest = join(vscode.workspace.rootPath, "bemg");
+
+                new TaskInit().copy(src, dest);
+
+                vscode.window.showInformationMessage("BEMG initialized successfully.");
+            } catch (error) {
+                vscode.window.showErrorMessage(error.message);
+            }
+        });
+
     const generate = vscode.commands.registerCommand('vs-bem-generate.bemGenerate',
-    async (uri: vscode.Uri) => {
-        const itemsOptions = { prompt: "Enter item names separated by space (__elem _mod_val ...etc)" };
-        const typesOptions = { prompt: "Enter types names separated by space (css tsx ...etc)" };
-        
-        const itemsInput = await vscode.window.showInputBox(itemsOptions) || '';
-        const typesInput = await vscode.window.showInputBox(typesOptions);
+        async (uri: vscode.Uri) => {
+            const task = new TaskGenerate(uri.path);
 
-        if(!typesInput) {
-            return;
-        }
+            const bemgScope = task._bemString;
+            const bemgTemplates = task._write._templates;
+            const bemgAlliases: { [key: string]: string } = task._write._aliases;
 
-        try {
-            const task = new Task(uri.path);
+            let selectedTypes: vscode.QuickPickItem[] = [];
 
-            task.write(typesInput.split(' '), itemsInput.split(' '));
-        } catch (error) {
-            vscode.window.showErrorMessage(error.message);
-        }
+            const itemsOptions = { prompt: "Enter item names separated by space (__elem _mod_val ...etc). Leave input empty or use one space for the block scope." };
+            const itemsInput = await vscode.window.showInputBox(itemsOptions) ?? '';
 
-    });
+            const bemgTemplatesNames = Object.keys(bemgTemplates);
+            let availableTypes: string[] = [];
 
+            itemsInput.split(' ').forEach(item => {
+                const bemType = bemNaming.typeOf(bemgScope + item);
+                bemgTemplatesNames.forEach(type => {
+                    // проверяем, доступен ли шаблон для типа сущности
+                    if (bemgTemplates[bemgAlliases[type] ?? type]?.[bemType]) {
+                        // добавляем в опции алиас или название шаблона
+                        availableTypes.push(Object.entries(bemgAlliases).find(([_key, value]) => value === type)?.[0] ?? type);
+                    }
+                });
+            });
+
+            // настраиваем QuickPick
+            const typesQuickPick = vscode.window.createQuickPick();
+            typesQuickPick.title = "Select entity types:";
+            typesQuickPick.items = [...new Set(availableTypes)].sort((a, b) => a.localeCompare(b)).map(template => ({ label: template, alwaysShow: true }));
+            typesQuickPick.canSelectMany = true;
+            typesQuickPick.onDidChangeSelection(selection => {
+                selectedTypes = selection;
+            });
+            typesQuickPick.onDidChangeValue(value => {
+                if (value === '') {
+                    return;
+                }
+
+                // если ввод совпадает с label одной из опций - отмечаем такую опцию как selected
+                let newItems: vscode.QuickPickItem[] = [];
+                value.split(' ').forEach(item => {
+                    const newItem = typesQuickPick.items.find(qpItem => qpItem.label === item);
+                    newItem && newItems.push(newItem);
+                });
+
+                if (typesQuickPick.selectedItems.length !== newItems.length) {
+                    typesQuickPick.selectedItems = newItems;
+                }
+            });
+            typesQuickPick.onDidAccept(() => {
+                typesQuickPick.hide();
+
+                if (!selectedTypes?.length) {
+                    return;
+                }
+
+                try {
+                    task.write(selectedTypes.map(item => item.label), itemsInput.split(' '));
+                } catch (error) {
+                    vscode.window.showErrorMessage(error.message);
+                }
+            });
+            typesQuickPick.onDidHide(() => typesQuickPick.dispose());
+            typesQuickPick.show();
+        });
+
+    context.subscriptions.push(init);
     context.subscriptions.push(generate);
 }
 
