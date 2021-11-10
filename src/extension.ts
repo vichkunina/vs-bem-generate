a04ea8eabd9e55aac5424db12113a248a8353187
Sergey Pushkin <pushkinss@yandex-team.ru>
2021-11-10 16:58:12 +0300

    Обновляет bemg до 2.0.0 версии (#5)

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,14 @@
+import { lstatSync, readFileSync } from 'fs';
+import { dirname } from 'path';
 import * as vscode from 'vscode';
 
-const TaskGenerate = require('bemg/lib/TaskGenerate');
+const Write = require('bemg/lib/generate/Write');
 const TaskInit = require('bemg/lib/TaskInit');
+const getConfigPaths = require('bemg/lib/getConfigPaths');
 const { join, resolve } = require("path");
 const bemNaming = require('bem-naming');
+const getBemRoot = require('bemg/lib/generate/getBemRoot');
+const getBemStringByPath = require('bemg/lib/generate/getBemStringByPath');
 
 export function activate(context: vscode.ExtensionContext) {
     const init = vscode.commands.registerCommand('vs-bem-generate.bemInit',
@@ -22,22 +27,36 @@ export function activate(context: vscode.ExtensionContext) {
 
     const generate = vscode.commands.registerCommand('vs-bem-generate.bemGenerate',
         async (uri: vscode.Uri) => {
-            const task = new TaskGenerate(uri.path);
+            const { configPath, templatesPath } = getConfigPaths(uri.path);
+            const bemgConfig = JSON.parse(readFileSync(configPath, { encoding: 'utf8' }));
 
-            const bemgScope = task._bemString;
-            const bemgTemplates = task._write._templates;
-            const bemgAlliases: { [key: string]: string } = task._write._aliases;
+            const stats = lstatSync(uri.path);
+            const directoryPath = stats.isDirectory() ? uri.path : dirname(uri.path);
 
-            let selectedTypes: vscode.QuickPickItem[] = [];
+            const bemString = getBemStringByPath(directoryPath, bemgConfig.naming);
 
-            const itemsOptions = { prompt: "Enter item names separated by space (__elem _mod_val ...etc). Leave input empty or use one space for the block scope." };
+            const bemgWrite = new Write(
+                getBemRoot(
+                    directoryPath,
+                    bemNaming.typeOf(bemString)
+                ),
+                bemgConfig,
+                templatesPath
+            );
+
+            const bemgTemplates = bemgWrite._templates;
+            const bemgAlliases: { [key: string]: string } = bemgWrite._config.aliases;
+
+            let selectedTypes: readonly vscode.QuickPickItem[] = [];
+
+            const itemsOptions = { prompt: "Enter item names separated by space or comma (__elem, __elem2 _mod_val ...etc). Leave input empty or use one space for the block scope." };
             const itemsInput = await vscode.window.showInputBox(itemsOptions) ?? '';
 
             const bemgTemplatesNames = Object.keys(bemgTemplates);
             let availableTypes: string[] = [];
 
             itemsInput.split(' ').forEach(item => {
-                const bemType = bemNaming.typeOf(bemgScope + item);
+                const bemType = bemNaming.typeOf(bemString + item);
                 bemgTemplatesNames.forEach(type => {
                     // проверяем, доступен ли шаблон для типа сущности
                     if (bemgTemplates[bemgAlliases[type] ?? type]?.[bemType]) {
@@ -62,7 +81,7 @@ export function activate(context: vscode.ExtensionContext) {
 
                 // если ввод совпадает с label одной из опций - отмечаем такую опцию как selected
                 let newItems: vscode.QuickPickItem[] = [];
-                value.split(' ').forEach(item => {
+                value.split(/[ ,]+/).forEach(item => {
                     const newItem = typesQuickPick.items.find(qpItem => qpItem.label === item);
                     newItem && newItems.push(newItem);
                 });
@@ -79,7 +98,8 @@ export function activate(context: vscode.ExtensionContext) {
                 }
 
                 try {
-                    task.write(selectedTypes.map(item => item.label), itemsInput.split(' '));
+                    const items = itemsInput.split(/[ ,]+/).map(item => bemString + item);
+                    items.forEach(item => bemgWrite.write(item, selectedTypes.map(item => item.label)));
                 } catch (error) {
                     vscode.window.showErrorMessage(error.message);
                 }
@@ -92,4 +112,4 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(generate);
 }
 
-export function deactivate() {}
+export function deactivate() { }
